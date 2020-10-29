aa6219f131a6b42b343fdc2cb5db2fb0f768c3c1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-29 09:19:35 +0200

    Initial Commit

---
+++ EnvironmentVariablesInSwiftUI/EnvironmentVariablesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentVariablesInSwiftUIApp.swift
+//  EnvironmentVariablesInSwiftUI
+//
+//  Created by Ramill Ibragimov on 29.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentVariablesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
